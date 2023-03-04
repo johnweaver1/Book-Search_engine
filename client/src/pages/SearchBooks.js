cdfae5850f06a24a35058709e5288d25f49c7910
John W <john.weaver94@utexas.edu>
2023-03-04 14:28:03 -0600

    search book changed input

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -79,7 +79,7 @@ const SearchBooks = () => {
 
     try {
       await saveBook({
-        variables: { newBook: { ...bookToSave } },
+        variables: { input: bookToSave },
       });
 
       // if book successfully saves to user's account, save book id to state
