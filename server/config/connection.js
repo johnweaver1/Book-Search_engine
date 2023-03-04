042ebc7100491a0c9f4e2b95b0e871fee4d345d1
John W <john.weaver94@utexas.edu>
2023-03-04 17:19:17 -0600

    removed controller

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookData'
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false,
+  useFindAndModify: true,
 });
 
 module.exports = mongoose.connection;
