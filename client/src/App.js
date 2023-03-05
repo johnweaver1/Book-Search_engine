a4f29c8a4819c8388bf25a197091a378fe29bbe3
John W <john.weaver94@utexas.edu>
2023-03-05 13:42:54 -0600

    changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,10 +27,13 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route exact path='/' component={<SearchBooks/>} />
-          <Route exact path='/saved' component={<SavedBooks/>} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route
+              path="*"
+              element={<h1 className="display-2">Wrong page!</h1>}
+            />
+          </Routes>
       </>
     </Router>
     </ApolloProvider>
